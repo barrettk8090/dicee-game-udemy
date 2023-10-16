66bcc98b9f1d8a3dcd4f91c19bffc4466c02d8cd
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-10-16 12:30:53 -0600

    update h1 to announce winner on refresh

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 +".png");
 document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 +".png");
+
+function determineWinner(){
+    if (randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "⬅️ Player 1 Wins!"
+    } else if (randomNumber2 > randomNumber1){
+        document.querySelector("h1").innerHTML = "➡️ Player 2 Wins!"
+    } else if (randomNumber1 === randomNumber2){
+        document.querySelector("h1").innerHTML = "It's A Draw! 😳"
+    }
+}
+
+determineWinner();
